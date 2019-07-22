68d7e2bf01906b06a0bff33a388204985094429a
Nabendu Karmakar <nabendu.karmakar@fractalanalytics.com>
2019-07-22 16:46:19 +0530

    Shell Again

--- deploy.sh
+++ deploy.sh
@@ -78,13 +78,14 @@ if [ -e "$DEPLOYMENT_TARGET/antenv3.6" ]; then
   echo "Found compatible virtual environment"
 else
   echo "Creating virtual environment."
-  /opt/python/3.6.8/bin/python3 -m pip install virtualenv
-  /opt/python/3.6.8/bin/python3 -m virtualenv antenv3.6
+  /opt/python/3.6.8/bin/python3 -m pip install virtualenv --user
+  /opt/python/3.6.8/bin/python3 -m virtualenv antenv3.6 --user
+  cd - > /dev/null
 fi
 
 # Install packages
 echo "Pip install requirements."
-/opt/python/3.6.8/bin/python3 -m pip install --upgrade -r requirements.txt
+/opt/python/3.6.8/bin/python3 -m pip install --upgrade -r requirements.txt --user
 
 # 2. KuduSync
 if [[ "$IN_PLACE_DEPLOYMENT" -ne "1" ]]; then
