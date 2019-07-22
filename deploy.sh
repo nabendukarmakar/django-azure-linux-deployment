bd0410098eb3c480e479a4a8180bf559e4f83385
Nabendu Karmakar <nabendu.karmakar@fractalanalytics.com>
2019-07-22 17:09:11 +0530

    Re-kudo Sync

--- deploy.sh
+++ deploy.sh
@@ -87,12 +87,12 @@ else
   echo "Creating virtual environment."
   /opt/python/3.6.8/bin/python3 -m pip install virtualenv --user
   /opt/python/3.6.8/bin/python3 -m virtualenv antenv3.6 --user
-  cd - > /dev/null
+  source /home/site/wwwroot/antenv3.6/bin/activate
 fi
 
 # Install packages
 echo "Pip install requirements."
-/opt/python/3.6.8/bin/python3 -m pip install --upgrade -r requirements.txt --user
+python -m pip install --upgrade -r requirements.txt --user
 
 ##################################################################################################################################
 echo "Finished successfully."
