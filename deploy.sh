3aa77676ff88b9ebf4236b9f366cc7f7f5771d73
Nabendu Karmakar <nabendu.karmakar@fractalanalytics.com>
2019-07-30 19:18:34 +0530

    Re-try #5

--- deploy.sh
+++ deploy.sh
@@ -84,8 +84,8 @@ if [ -e "$DEPLOYMENT_TARGET/antenv3.6" ]; then
   echo "Found compatible virtual environment"
 else
   echo "Creating virtual environment."
-  /opt/python/3.7.3/bin/python3 -m pip install --upgrade --force-reinstall virtualenv --user
-  /opt/python/3.7.3/bin/python3 -m virtualenv antenv3.6
+  /opt/python/3.6.8/bin/python3 -m pip install --upgrade --force-reinstall virtualenv --user
+  /opt/python/3.6.8/bin/python3 -m virtualenv antenv3.6
   # /opt/python/3.6.8/bin/python3 -m venv antenv3.6
 fi
 
