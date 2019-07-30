1778baeae490e31110bc0e7d51930c707e91233d
Nabendu Karmakar <nabendu.karmakar@fractalanalytics.com>
2019-07-30 19:48:08 +0530

    My environment

--- deploy.sh
+++ deploy.sh
@@ -80,23 +80,23 @@ fi
 # 2. Install PIP Dependencies
 cd "$DEPLOYMENT_TARGET"
 
-if [ -e "$DEPLOYMENT_TARGET/antenv3.7" ]; then
-  echo "Found compatible virtual environment"
-else
-  echo "Creating virtual environment."
-  /opt/python/3.6.8/bin/python3 -m pip install --upgrade --force-reinstall virtualenv --user
-  /opt/python/3.6.8/bin/python3 -m virtualenv antenv3.7
-  # /opt/python/3.6.8/bin/python3 -m venv antenv3.6
-fi
+# if [ -e "$DEPLOYMENT_TARGET/antenv3.7" ]; then
+#   echo "Found compatible virtual environment"
+# else
+#   echo "Creating virtual environment."
+#   /opt/python/3.6.8/bin/python3 -m pip install --upgrade --force-reinstall virtualenv --user
+#   /opt/python/3.6.8/bin/python3 -m virtualenv antenv3.7
+#   # /opt/python/3.6.8/bin/python3 -m venv antenv3.6
+# fi
 
-echo "Activating virtual environment"
-source $DEPLOYMENT_TARGET/antenv3.7/bin/activate
+# echo "Activating virtual environment"
+# source $DEPLOYMENT_TARGET/antenv3.7/bin/activate
 
-# Install packages
-echo "Pip install requirements."
+# # Install packages
+# echo "Pip install requirements."
 
-python3 -m pip install -U pip
-python3 -m pip install -r requirements.txt
+# python3 -m pip install -U pip
+# python3 -m pip install -r requirements.txt
 
 ##################################################################################################################################
 echo "Finished successfully."
---
+++ nabendu/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/fractaluser/Documents/Workspace/Project Code - Workspace/django-azure-linux-deployment/antenv3.6/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
